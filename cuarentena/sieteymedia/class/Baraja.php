6fe8adc316f4d47a280af4f1bb27fe3a646825e5
Fco Javier Glez <FJGS_86@hotmail.com>
2020-04-15 19:48:39 +0200

    Añadido recurso 0.jpg que representa una carta bocaabajo

--- cuarentena/sieteymedia/class/Baraja.php
+++ cuarentena/sieteymedia/class/Baraja.php
@@ -10,9 +10,14 @@
                 array_push($this->_mazo,new Carta(intval($i/10),(($i+1)%10==0) ? 10 : (($i+1)%10)));
         }
 
+        /**
+         * Imprime en pantalla todas las cartas que componen actualmente la baraja la baraja
+         */
         public function mostrarBaraja() {
+            //array_splice($this->_mazo,10,1);
             for ($i=0; $i<sizeof($this->_mazo); $i++)
-                echo "<img src=".$this->_mazo[$i]->getUrlImg()." alt=".$this->_mazo[$i]->getNombreCarta()." title=".$this->_mazo[$i]->getNombreCarta().">";
+                echo "<img src=".$this->_mazo[$i]->getUrlImg()." alt=".$this->_mazo[$i]->getNombreCarta()." 
+                        title=".$this->_mazo[$i]->getNombreCarta().">";
         }
     }
 ?>
