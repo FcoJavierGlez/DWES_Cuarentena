923515b1c2ff155144043bdb0f25053fea6edf9a
Fco Javier Glez <FJGS_86@hotmail.com>
2020-04-17 14:31:27 +0200

    Añadido normalize.css

--- cuarentena/ejercicio5/index4.php
+++ cuarentena/ejercicio5/index4.php
@@ -40,6 +40,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Barcos formulario</title>
 </head>
 <body>
@@ -64,8 +66,12 @@
         ?>
     </main>
     <footer>
-
+        <h4>RRSS del autor:</h4>
+        <div class="rrss">
+            <a href="https://twitter.com/Fco_Javier_Glez" target="_blank"><img src="img/twitter.png" alt="Enlace a cuenta de Twitter del autor"></a>
+            <a href="https://github.com/FcoJavierGlez" target="_blank"><img src="img/github.png" alt="Enlace a cuenta de GitHub del autor"></a>
+            <a href="https://www.linkedin.com/in/francisco-javier-gonz%C3%A1lez-sabariego-51052a175/" target="_blank"><img src="img/linkedin.png" alt="Enlace a cuenta de Linkedin del autor"></a>
+        </div>
     </footer>
-    
 </body>
 </html>
