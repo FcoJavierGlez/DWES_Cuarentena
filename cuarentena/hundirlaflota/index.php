c39581908c5bd52c8961e6bfb76b77c5ad1ac0af
Fco Javier Glez <FJGS_86@hotmail.com>
2020-04-18 21:24:35 +0200

    Actualizado enlace a GitHub

--- cuarentena/hundirlaflota/index.php
+++ cuarentena/hundirlaflota/index.php
@@ -59,7 +59,7 @@
     <main>
         <div class="github">
             <b>El siguiente botón conduce al repositorio de GitHub:</b>
-            <a href="https://github.com/FcoJavierGlez/DWES_Cuarentena/tree/barcosVerFinal/cuarentena/ejercicio5" target="_blank">
+            <a href="https://github.com/FcoJavierGlez/DWES_Cuarentena/tree/hundirlaflota/cuarentena/hundirlaflota" target="_blank">
                 <button>Ver código</button>
             </a>
         </div>
