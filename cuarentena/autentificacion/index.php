9f67eeac4bfd49762ae42e8889ffa1fa5ff954b4
FcoJavierGlez <FJGS_86@hotmail.com>
2020-04-23 20:41:33 +0200

    Añadido enlace al repositorio

--- cuarentena/autentificacion/index.php
+++ cuarentena/autentificacion/index.php
@@ -40,7 +40,7 @@
     <main>
         <div class="github">
             <b>El siguiente botón conduce al repositorio de GitHub:</b>
-            <a href="#" target="_blank">
+            <a href="https://github.com/FcoJavierGlez/DWES_Cuarentena/tree/autentificacionFicheros/cuarentena/autentificacion" target="_blank">
                 <button>Ver código</button>
             </a>
         </div>
