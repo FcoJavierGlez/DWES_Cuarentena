c90f4afeb31cb9f5e93e1cbcefb24b5025ce3d6b
FcoJavierGlez <FJGS_86@hotmail.com>
2020-04-23 12:48:45 +0200

    Añadida estructura básica en index.php

--- cuarentena/autentificacion/index.php
+++ cuarentena/autentificacion/index.php
@@ -4,9 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Autentificación</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Autentificación</h1>
+    </header>
+    <nav>
+        <!-- menú -->
+    </nav>
+    <main>
+       <!--  estructura de main -->
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
