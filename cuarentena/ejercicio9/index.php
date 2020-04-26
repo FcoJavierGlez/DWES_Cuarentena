124b8c3ecf0e1f6bdd9bf5066a3ecc2fda9a55e0
FcoJavierGlez <FJGS_86@hotmail.com>
2020-04-26 19:32:45 +0200

    Preparada la estructura básica de index.php y su hoja de estilos

--- cuarentena/ejercicio9/index.php
+++ cuarentena/ejercicio9/index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Francisco Javier González Sabariego">
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
     <title>Generación de usuarios</title>
 </head>
@@ -11,10 +13,21 @@
         <h1>Generación de usuarios</h1>
     </header>
     <main>
-
+        <div class="github">
+            <b>El siguiente botón conduce al repositorio de GitHub:</b>
+            <a href="#" target="_blank">
+                <button>Ver código</button>
+            </a>
+        </div>
+        <?php?>
     </main>
     <footer>
-        
+        <h4>RRSS del autor:</h4>
+        <div>
+            <a href="https://twitter.com/Fco_Javier_Glez" target="_blank"><img src="img/twitter.png" alt="Enlace a cuenta de Twitter del autor"></a>
+            <a href="https://github.com/FcoJavierGlez" target="_blank"><img src="img/github.png" alt="Enlace a cuenta de GitHub del autor"></a>
+            <a href="https://www.linkedin.com/in/francisco-javier-gonz%C3%A1lez-sabariego-51052a175/" target="_blank"><img src="img/linkedin.png" alt="Enlace a cuenta de Linkedin del autor"></a>
+        </div>
     </footer>
 </body>
 </html>
