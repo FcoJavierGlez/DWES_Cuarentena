db89c56498833c153104cd7f57f118af62fd4f16
FcoJavierGlez <FJGS_86@hotmail.com>
2020-04-26 19:22:42 +0200

    Primer commit, movido y renombrado index.php a resources/funciones.php

--- cuarentena/ejercicio9/index.php
+++ cuarentena/ejercicio9/index.php
@@ -1,39 +1,20 @@
-<?php
-    function getUser($nombre) {
-        return substr(normalizaCadena(explode(" ",$nombre)[0]),0,2).substr(normalizaCadena(explode(" ",$nombre)[1]),0,2).substr(normalizaCadena(explode(" ",$nombre)[2]),0,2);
-    }
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Generación de usuarios</title>
+</head>
+<body>
+    <header>
+        <h1>Generación de usuarios</h1>
+    </header>
+    <main>
 
-    function normalizaCadena($cadena) {
-        $acentos = array("á","é","í","ó","ú","Á","É","Í","Ó","Ú","Ñ","ñ");
-        $letras = array("a","e","i","o","u","a","e","i","o","u","n","n");
-        return str_replace($acentos, $letras, strtolower($cadena));
-    }
-
-    $usuarios = array();
-
-    $file = fopen("Alumnos.txt", "r") or exit("Unable to open file!");
-    //Output a line of the file until the end is reached
-
-    $i = 0;
-
-    if ($file) {
-        while (($line = fgets($file)) !== false) {
-            if ($i>7) {
-                $numUser = 0;
-                if (in_array(getUser($line),$usuarios)) {
-                    do {
-                        $numUser++;
-                    } while (in_array(getUser($line).$numUser,$usuarios));
-                    array_push($usuarios,getUser($line).$numUser);
-                } else 
-                    array_push($usuarios,getUser($line));
-            }
-            $i++;
-        }
-    } else
-        echo "El fichero no pudo ser abierto.";
-
-    //imprimir lista de usuarios:
-    for ($i=0; $i<sizeof($usuarios); $i++) 
-        echo $usuarios[$i]."<br/>";
-?>
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
