3ebc3d5438ae12275674baa1fddc4e2b0f036cf5
FcoJavierGlez <FJGS_86@hotmail.com>
2020-05-04 11:43:06 +0200

    Añadido enlace a GitHub

--- cuarentena/basicos3/index.php
+++ cuarentena/basicos3/index.php
@@ -30,7 +30,7 @@
     <main>
         <div class="github">
             <b>El siguiente botón conduce al repositorio de GitHub:</b>
-            <a href="#" target="_blank">
+            <a href="https://github.com/FcoJavierGlez/DWES_Cuarentena/tree/basicos3/cuarentena/basicos3" target="_blank">
                 <button>Ver código</button>
             </a>
         </div>
