29b0129d0755205bd37ca29ffb207d4a8569942b
FcoJavierGlez <FJGS_86@hotmail.com>
2020-05-13 14:56:16 +0200

    Modificada clase Pila para comportarse como una Cola

--- cuarentena/basicos4/class/Pila.php
+++ cuarentena/basicos4/class/Pila.php
@@ -1,7 +1,7 @@
 <?php
     include "resources/funciones.php";
 
-    class Pila {
+    class Cola {
 
         private $_array = array();
 
@@ -10,48 +10,48 @@
         }
 
         /**
-         * Añade un elemento arriba de la pila
+         * Añade un elemento a la cola
          */
         public function addElement($a) {
             if (sizeof($this->_array)==10) throw new Exception();   //Tope de 10 elementos, suficiente para ver su funcionamiento
             $a = limpiarDatos($a);
-            array_unshift($this->_array,$a);
+            array_push($this->_array,$a);
         }
 
         /**
-         * Elimina el elemento de arriba de la pila
+         * Elimina el elemento de arriba de la cola
          */
         public function delElement() {
             array_shift($this->_array);
         }
 
         /**
-         * Borra el contenido de la pila
+         * Borra el contenido de la cola
          */
         public function clear() {
             array_splice($this->_array,0,sizeof($this->_array));
         }
 
         /**
-         * Devuelve el elemento de arriba de la pila
+         * Devuelve el elemento de arriba de la cola
          */
         public function getElement() {
             return $this->_array[0];
         }
 
         /**
-         * Devuelve el total de elementos que contiene la pila
+         * Devuelve el total de elementos que contiene la cola
          */
         public function getNumElements() {
             return sizeof($this->_array);
         }
 
         /**
-         * Imprime contenido de la pila
+         * Imprime contenido de la cola
          */
         public function __toString() {
             //print_r($this->_array);
-            echo "<b>Elementos de la pila:</b>"."<br>";
+            echo "<b>Elementos de la cola:</b>"."<br>";
             for ($i=0; $i<sizeof($this->_array); $i++) 
                 echo $this->_array[$i]."<br/>";
         }
