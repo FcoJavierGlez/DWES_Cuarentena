8830252a9b151993361d5216431051574137675e
FcoJavierGlez <FJGS_86@hotmail.com>
2020-05-13 15:47:17 +0200

    Añadida nuevamente la clase Pila.php

---
+++ cuarentena/basicos4/class/Pila.php
@@ -0,0 +1,59 @@
+<?php
+    include "resources/funciones.php";
+
+    class Pila {
+
+        private $_array = array();
+
+        public function __construct() {
+
+        }
+
+        /**
+         * Añade un elemento arriba de la pila
+         */
+        public function addElement($a) {
+            if (sizeof($this->_array)==10) throw new Exception();   //Tope de 10 elementos, suficiente para ver su funcionamiento
+            $a = limpiarDatos($a);
+            array_unshift($this->_array,$a);
+        }
+
+        /**
+         * Elimina el elemento de arriba de la pila
+         */
+        public function delElement() {
+            array_shift($this->_array);
+        }
+
+        /**
+         * Borra el contenido de la pila
+         */
+        public function clear() {
+            array_splice($this->_array,0,sizeof($this->_array));
+        }
+
+        /**
+         * Devuelve el elemento de arriba de la pila
+         */
+        public function getElement() {
+            return $this->_array[0];
+        }
+
+        /**
+         * Devuelve el total de elementos que contiene la pila
+         */
+        public function getNumElements() {
+            return sizeof($this->_array);
+        }
+
+        /**
+         * Imprime contenido de la pila
+         */
+        public function __toString() {
+            //print_r($this->_array);
+            echo "<b>Elementos de la pila:</b>"."<br>";
+            for ($i=0; $i<sizeof($this->_array); $i++) 
+                echo $this->_array[$i]."<br/>";
+        }
+    }
+?>
