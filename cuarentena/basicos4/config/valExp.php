015be0bb524257625f19f65d8713f6d27f0066b0
FcoJavierGlez <FJGS_86@hotmail.com>
2020-05-14 13:36:59 +0200

    Terminado ejercicio validar expresión

--- cuarentena/basicos4/config/valExp.php
+++ cuarentena/basicos4/config/valExp.php
@@ -1,13 +1,22 @@
 <?php
     echo "<form action=".$_SERVER['PHP_SELF']." method='post'>";
-        if (!isset($_POST['valida_exp']))
-            echo "Validar expresión:  <input type='submit' value='Validar' name='gen_cartas'><br/><br/>";
-
-        elseif (isset($_POST['valida_exp'])) {                 //Si se pulsa el botón "Generar cartas"
-            //$_SESSION['cartas']->generarCartas();
-            echo "<div>";
-                //$_SESSION['cartas']->imprimeEnlaces();
-            echo "</div>";
+        if ( !isset($_POST['valida_exp']) ) {
+            echo "Validar expresión:  <input type='text' value='' name='expresion'>";
+            echo "<input type='submit' value='Validar' name='valida_exp'><br/><br/>";
+        }
+        elseif ( isset($_POST['valida_exp']) ) {                 //Si se pulsa el botón para validar
+            if ( !empty($_POST['expresion']) ) {
+                echo "Validar expresión:  <input type='text' value=".str_replace( " ", "", $_POST['expresion'] )." name='expresion'>";
+                echo "<input type='submit' value='Validar' name='valida_exp'><br/><br/>";
+                if ( $_SESSION['exp']->valida($_POST['expresion']) )
+                    echo "Expresión válida.";
+                else
+                    echo "<p class='mensaje_error'>Expresión inválida.</p>";
+            }
+            else {
+                echo "Validar expresión:  <input type='text' value='' name='expresion'>";
+                echo "<input type='submit' value='Validar' name='valida_exp'><br/><br/>";
+            }
         }
     echo "</form>";
 ?>
