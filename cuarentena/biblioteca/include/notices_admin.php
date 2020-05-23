bbf74d498ae31becf0ec0b09bdfe212140bb3cc6
FcoJavierGlez <FJGS_86@hotmail.com>
2020-05-23 18:06:55 +0200

    Añadidas notificaciones para administradores

---
+++ cuarentena/biblioteca/include/notices_admin.php
@@ -0,0 +1,67 @@
+<div>
+    <h3>NOTICIAS ADMINISTRACIÓN</h3>
+</div>
+<div class='filtro'>
+    
+</div>
+<div class='add_editar scroll'>
+    <div class="ficha_prestamo w90 pergamino">
+        <article>
+            <h4>Actualización del sistema</h4>
+            <p>
+                Recordamos a los administradores que el próximo viernes, 29 de mayo de 2020, a las 00:00 horas, se procederá 
+                a actualizar el sistema de gestión de correos electrónicos. Por tanto y, conforme se habló en la última reunión, 
+                será necesario proceder a comprobar que las copias de seguridad de la base de datos están preparadas para 
+                posibles fallos post-actualización.
+            </p>
+        </article>
+        <article>
+            <h4>Noticia 1</h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, 
+                ipsa repellat velit commodi consequatur ullam accusamus hic rem dolor ex vero, 
+                deserunt assumenda, enim blanditiis aut iusto minus temporibus impedit.
+            </p>
+        </article>
+        <article>
+            <h4>Noticia 2</h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, 
+                ipsa repellat velit commodi consequatur ullam accusamus hic rem dolor ex vero, 
+                deserunt assumenda, enim blanditiis aut iusto minus temporibus impedit.
+            </p>
+        </article>
+        <article>
+            <h4>Noticia 3</h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, 
+                ipsa repellat velit commodi consequatur ullam accusamus hic rem dolor ex vero, 
+                deserunt assumenda, enim blanditiis aut iusto minus temporibus impedit.
+            </p>
+        </article>
+        <article>
+            <h4>Noticia 4</h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, 
+                ipsa repellat velit commodi consequatur ullam accusamus hic rem dolor ex vero, 
+                deserunt assumenda, enim blanditiis aut iusto minus temporibus impedit.
+            </p>
+        </article>
+        <article>
+            <h4>Noticia 5</h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, 
+                ipsa repellat velit commodi consequatur ullam accusamus hic rem dolor ex vero, 
+                deserunt assumenda, enim blanditiis aut iusto minus temporibus impedit.
+            </p>
+        </article>
+        <article>
+            <h4>Noticia 6</h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, 
+                ipsa repellat velit commodi consequatur ullam accusamus hic rem dolor ex vero, 
+                deserunt assumenda, enim blanditiis aut iusto minus temporibus impedit.
+            </p>
+        </article>
+    </div>
+</div>
