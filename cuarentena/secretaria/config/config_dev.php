2173e83721e36a438570b52283dccc56bc0bda06
FcoJavierGlez <FJGS_86@hotmail.com>
2020-05-27 11:00:51 +0200

    Modificado fichero config_dev

--- cuarentena/secretaria/config/config_dev.php
+++ cuarentena/secretaria/config/config_dev.php
@@ -1,6 +1,6 @@
 <?php
     define("DBHOST","localhost");
-    define("DBNAME","biblioteca");
+    define("DBNAME","ex_secretariavirtual");
     define("DBPORT",3306);
     define("DBUSER","2daw1920");
     define("DBPASS","2daw1920");
