f131f660bb320537562fa8baac79f157868cb754
FcoJavierGlez <FJGS_86@hotmail.com>
2020-05-29 10:49:56 +0200

    Eliminados ficheros y carpetas del proyecto biblioteca

--- cuarentena/secretaria/class/error/IsbnExistException.php
+++
@@ -1,8 +0,0 @@
-<?php
-    /**
-     * Excepción que se lanza cuando un ISBN ya está registrado en el sistema
-     */
-    class IsbnExistException extends Exception {
-
-    }
-?>
--- cuarentena/secretaria/class/error/IsbnInvalidException.php
+++
@@ -1,8 +0,0 @@
-<?php
-    /**
-     * Excepción que se lanza cuando un ISBN no cumple con el formato del ISBN10 o ISBN13
-     */
-    class IsbnInvalidException extends Exception {
-
-    }
-?>
