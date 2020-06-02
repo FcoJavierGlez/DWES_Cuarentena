44e70ebf57da8b45d2dc9a11813580c0c46e4404
FcoJavierGlez <FJGS_86@hotmail.com>
2020-06-02 17:51:44 +0200

    Mejora de código

--- cuarentena/secretaria/include/documents/sign_document.php
+++ cuarentena/secretaria/include/documents/sign_document.php
@@ -2,6 +2,13 @@
     if ( !isset($_SESSION['user']) || !( $_SESSION['user']['perfil'] == "user" && $_SESSION['user']['estado'] == "activo" ) ) {
         header('Location:index.php');
     }
+
+    $documento = $_SESSION['documento']->getDocumentTosign( $_GET['firmar'], $_SESSION['user']['id'] );
+
+    $fil1 = rand(0, 7);
+    $col1 = rand(0, 7);
+    $fil2 = rand(0, 7);
+    $col2 = rand(0, 7);
 ?>
 <div>
     <h3>Firmar documento</h3>
@@ -13,30 +20,35 @@
     </form>  |  <a href="index.php?add_document"><button class="boton_sq aceptar">Nuevo documento</button></a> -->
 </div>
 <div class="add_editar scroll">
-    <div class="ficha_prestamo">
+    <div class="ficha_prestamo h90">
         <h4>Está a punto de firmar el siguiente documento:</h4>
         <?php
-            imprimeFichaDocumento( $_SESSION['documento']->getDocumentTosign( $_GET['firmar'], $_SESSION['user']['id'] ) );
+            imprimeFichaDocumento( $documento );
         ?>
-        <h4>Para firmarlo deberá utilizar su clave de firmas personal.</h4>
-        <form action="index.php?documentos" method="post"><!-- echo $_SERVER['PHP_SELF'] -->
+        <form action="index.php?documentos" method="post">
             <?php
-                $fil1 = rand(0, 7);
-                $col1 = rand(0, 7);
-                $fil2 = rand(0, 7);
-                $col2 = rand(0, 7);
-                echo "Introduzca el valor en ".($col1+1).getLetterFromRow( $fil1 ).": 
-                        <input type='text' value='' name="."input[".$fil1."][".$col1."]"."><br/>";
-                echo "Introduzca el valor en ".($col2+1).getLetterFromRow( $fil2 ).": 
-                        <input type='text' value='' name="."input[".$fil2."][".$col2."]"."><br/>";
+                if ( sizeof($documento) ) {
+                    echo "<h4>Para firmarlo deberá utilizar su clave de firmas personal.</h4>";
+                    echo "Introduzca el valor en ".($col1+1).getLetterFromRow( $fil1 ).": 
+                            <input type='text' value='' name="."input[".$fil1."][".$col1."]"."><br/>";
+                    echo "Introduzca el valor en ".($col2+1).getLetterFromRow( $fil2 ).": 
+                            <input type='text' value='' name="."input[".$fil2."][".$col2."]"."><br/>";
+                } else {
+                    echo "<b><span class='bloqueado justify_center'>SOLICITUD DENEGADA.</span></b><br/>";
+                    echo "<b><span class='bloqueado'>No posee ningún documento de éstas características.</span></b>";
+                }
             ?>
             <input type="hidden" name="fecha" value="<?php echo date("Y-m-d") ?>">
             <input type="hidden" name="hora" value="<?php echo date("H:i:s") ?>">
             <input type="hidden" name="idFichero" value="<?php echo $_GET['firmar'] ?>">
-            <div class="pie_ficha c2">
-                <input type="submit" value="Aceptar" name="sign" class="boton_sq aceptar">
-                <input type="submit" value="Cancelar" name="" class="boton_sq cancelar">
-            </div>
+            <?php
+                if ( sizeof($documento) ) {
+                    echo "<div class='pie_ficha c2'>";
+                        echo "<input type='submit' value='Aceptar' name='sign' class='boton_sq aceptar'>";
+                        echo "<input type='submit' value='Cancelar' name='' class='boton_sq cancelar'>";
+                    echo "</div>";
+                }
+            ?>
         </form>
     </div>
 </div>
